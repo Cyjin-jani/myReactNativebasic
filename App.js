5d9b34607cf39bf5522a96d0dd6c87ae5b173936
yjcho <jinjanic91@gmail.com>
2021-05-14 00:06:07 +0900

    scrollView and list

--- App.js
+++ App.js
@@ -1,10 +1,26 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, Button, TextInput } from 'react-native';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Button,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 export default function App() {
   const [name, setName] = useState('kaka');
   const [person, setPerson] = useState({ name: 'mario', age: 40 });
+  const [people, setPeople] = useState([
+    { name: 'DK', key: '1' },
+    { name: 'RNG', key: '2' },
+    { name: 'MAL', key: '3' },
+    { name: 'C9', key: '4' },
+    { name: 'RNG', key: '5' },
+    { name: 'MAL', key: '6' },
+    { name: 'C9', key: '7' },
+  ]);
 
   const clickHandler = () => {
     setName('ricardo');
@@ -15,11 +31,11 @@ export default function App() {
     <View style={styles.container}>
       <StatusBar style="auto" />
 
-      <View style={styles.header}>
+      {/* <View style={styles.header}>
         <Text style={styles.boldText}> Hello world! {name} </Text>
-        {/* <Text>
+        <Text>
           His name is {person.name} and his age is {person.age}{' '}
-        </Text> */}
+        </Text>
         <Text>Enter Name:</Text>
         <TextInput
           style={styles.input}
@@ -28,9 +44,18 @@ export default function App() {
           keyboardType="numeric"
           multiline
         />
-      </View>
-      <View style={styles.buttonContainer}>
+      </View> */}
+      {/* <View style={styles.buttonContainer}>
         <Button title="update Name" onPress={clickHandler} />
+      </View> */}
+      <View style={styles.containerBox}>
+        <ScrollView>
+          {people.map((person) => (
+            <View key={person.key}>
+              <Text style={styles.item}>{person.name}</Text>
+            </View>
+          ))}
+        </ScrollView>
       </View>
     </View>
   );
@@ -40,8 +65,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // alignItems: 'center',
+    // justifyContent: 'center',
+  },
+  containerBox: {
+    flex: 1,
+    backgroundColor: '#fff',
+    paddingTop: 40,
+    paddingHorizontal: 20,
   },
   header: {
     backgroundColor: 'yellow',
@@ -62,5 +93,11 @@ const styles = StyleSheet.create({
     padding: 8,
     margin: 10,
     width: 200,
+  },
+  item: {
+    marginTop: 24,
+    padding: 30,
+    backgroundColor: 'pink',
+    fontSize: 24,
   },
 });
