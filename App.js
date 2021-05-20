f7d9aa1db67d849aa8f289af0b12482a6ac37a0b
yjcho <jinjanic91@gmail.com>
2021-05-21 00:20:40 +0900

    custom fonts apply to app

--- App.js
+++ App.js
@@ -1,8 +1,28 @@
 import { StatusBar } from 'expo-status-bar';
 import React, { useState } from 'react';
+import * as Font from 'expo-font';
 
 import Home from './screens/home';
 
+import AppLoading from 'expo-app-loading';
+
+const getFonts = () =>
+  Font.loadAsync({
+    'HomemadeApple-Regular': require('./assets/fonts/HomemadeApple-Regular.ttf'),
+  });
+
 export default function App() {
-  return <Home />;
+  const [fontLoaded, setFontLoaded] = useState(false);
+
+  if (fontLoaded) {
+    return <Home />;
+  } else {
+    return (
+      <AppLoading
+        startAsync={getFonts}
+        onFinish={() => setFontLoaded(true)}
+        onError={() => console.log('err')}
+      />
+    );
+  }
 }
